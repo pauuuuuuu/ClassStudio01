664b6228aa09d5f0a82eddb766f08475bca1c465
Paula Sanchez <41597298+pauuuuuuu@users.noreply.github.com>
2021-07-31 12:00:22 +0000

    test

--- index.js
+++ index.js
@@ -64,3 +64,46 @@ let grade() {
 //   });
 // })
 
+// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
+//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+
+describe("Command class", function() {
+
+  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+    expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+  });
+
+  it("Command type required", function() {
+    let command = new Command('STATUS_CHECK');
+    expect(command.commandType).toEqual('STATUS_CHECK');
+  });
+
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    let command = new Command('MOVE', 20);
+    expect(command.value).toEqual(20);
+  });
+
+});
+
+describe("Test 2: ", function() {
+  //first try of the test.
+  it("Constructor sets command type -> ", function() {
+    let command = new Command('UNKNOWN');
+    //expect(command.commandType).not.toEqual('');
+    //if (command.commandType) {
+      //console.log("Command type requiredd");
+      //return 0;
+    //};
+  });
+});
+
+describe("Test 3: ", function(){
+  it("Constructor sets a value passed in as the 2nd argument -> ", function(){
+    let command = new Command('UNKNOWN', 999);
+//to try the value property we us next 2 lines we declare numbers between 0<>1001    
+    expect(command.value).toBeGreaterThan(0);
+    expect(command.value).toBeLessThan(1001);
+  });
+})
+
+
